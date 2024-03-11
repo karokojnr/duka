cd1088119f634ddaedeb1ddf77a0d1e1f897e68b
karokojnr <karoko.jnr99@gmail.com>
2024-03-11 15:16:57 +0300

    Set up folder structure & installed Fiber

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	fmt.Println("Welcome to Duka")
+
+	app := fiber.New()
+
+	// routes
+
+	app.Listen("localhost:4000")
 }
