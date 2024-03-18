f33843928530739d76bc5a5011d02efcaccf36ad
karokojnr <karoko.jnr99@gmail.com>
2024-03-18 14:50:06 +0300

    Set up user handlers

--- internal/api/server.go
+++ internal/api/server.go
@@ -3,14 +3,17 @@ package api
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/karokojnr/duka/config"
+	"github.com/karokojnr/duka/internal/api/rest"
+	"github.com/karokojnr/duka/internal/api/rest/handlers"
 	"log"
-	"net/http"
 )
 
 func StartServer(config config.AppConfig) {
 	app := fiber.New()
-
-	app.Get("/health", healthCheck)
+	handler := &rest.RestHandler{
+		App: app,
+	}
+	setUpHandlers(handler)
 
 	err := app.Listen(config.Port)
 	if err != nil {
@@ -18,8 +21,11 @@ func StartServer(config config.AppConfig) {
 	}
 }
 
-func healthCheck(ctx *fiber.Ctx) error {
-	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
-		"message": "Server running",
-	})
+func setUpHandlers(handler *rest.RestHandler) {
+	// User handler
+	handlers.SetupUserRoutes(handler)
+
+	// todo: Transactions handler
+
+	// todo: Catalog handler
 }
