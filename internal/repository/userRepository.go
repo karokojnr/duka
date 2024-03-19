cb2c5028a3136465d7542c9528c0544fe10105a8
karokojnr <karoko.jnr99@gmail.com>
2024-03-19 23:37:17 +0300

    Implemented user repository

--- internal/repository/userRepository.go
+++ internal/repository/userRepository.go
@@ -1,13 +1,33 @@
 package repository
 
-import "os/user"
+import (
+	"github.com/karokojnr/duka/internal/domain"
+	"gorm.io/gorm"
+)
 
 type UserRepository interface {
-	CreateUser(usr user.User) (user.User, error)
-	GetUser(email string) (user.User, error)
-	GetUserById(id uint) (user.User, error)
-	UpdateUser(id uint, usr user.User) (user.User, error)
+	CreateUser(usr domain.User) (domain.User, error)
+	GetUser(email string) (domain.User, error)
+	GetUserById(id uint) (domain.User, error)
+	UpdateUser(id uint, usr domain.User) (domain.User, error)
 }
 
 type userRepository struct {
+	db *gorm.DB
+}
+
+func (r userRepository) CreateUser(usr domain.User) (domain.User, error) {
+	return domain.User{}, nil
+}
+
+func (r userRepository) GetUser(email string) (domain.User, error) {
+	return domain.User{}, nil
+}
+
+func (r userRepository) GetUserById(id uint) (domain.User, error) {
+	return domain.User{}, nil
+}
+
+func (r userRepository) UpdateUser(id uint, usr domain.User) (domain.User, error) {
+	return domain.User{}, nil
 }
